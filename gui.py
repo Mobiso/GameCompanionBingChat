8c396df2f8f5eea5738eb8458762d3fe39cf6dab
Mobiso <95570641+Mobiso@users.noreply.github.com>
2023-06-25 16:00:58 +0200

    window closes after asked question
    the ask window closes after being asked a question. Answer windows opens when answer is ready.
    
    You are currently able to open a new window while waiting for response.

--- gui.py
+++ gui.py
@@ -7,33 +7,55 @@ import keyboard
 import json
 
 
-def ask():
-    question = e1.get()
+def ask(software, question):
+    print("in ask")
     if question is not None:
         response = asyncio.run(askBing.askBing(software, question))
         for key in response:
             if key == "text":
-                T.insert(tk.END, response[key])
+                return response[key]
 
 
-def close():
-    master.destroy()
+def close(window):
+    window.destroy()
+
+
+def answerWindow(response, question):
+    answerWindow = tk.Tk()
+    answerWindow.attributes('-topmost', True)
+    answerWindow.title("Bing answer!")
+    tk.Label(answerWindow, text="Question:").grid(row=0, column=0)
+    tk.Label(answerWindow, text="Answer:").grid(row=1, columnspan=2)
+    tQuestion = tk.Text(answerWindow, height=1, width=40)
+    tQuestion.grid(row=0, column=1)
+    tAnswer = tk.Text(answerWindow, height=40, width=100)
+    tAnswer.grid(row=1, rowspan=2)
+    tQuestion.insert(tk.END, question)
+    tAnswer.insert(tk.END, response)
+    answerWindow.mainloop()
+
+
+def middleHand(window, processName, question):
+    print("Entered middleHand")
+    close(window)
+    answerWindow(ask(processName, question),question)
 
 
 programRunning = True
 while programRunning:
+    print("Started")
     keyboard.wait("k")
-    software = activeWindow.active_window_process_name()
+    print("K pressed")
     master = tk.Tk()
-    master.attributes('-topmost',True)
+    print("Master createdk")
+    master.attributes('-topmost', True)
     master.title("Ask Bing!")
     tk.Label(master, text="Question").grid(row=0)
     e1 = tk.Entry(master)
-    b1 = tk.Button(master, text="Ask!", command=ask)
-    b2 = tk.Button(master, text="Quit", command=close)
-    T = tk.Text(master, height=10, width=70)
-    T.grid(row=2, column=0)
-    e1.grid(row=0, column=1)
-    b1.grid(row=1, column=0)
-    b2.grid(row=1, column=1)
+    print("next is b1")
+    b1 = tk.Button(master, text="Ask!", command=lambda :[middleHand(master, activeWindow.active_window_process_name(), e1.get())])
+    # b2 = tk.Button(master, text="Quit", command=close(master))
+    e1.grid(row=0, column=1, columnspan=2)
+    b1.grid(row=1, column=0, columnspan=2)
+    # kb2.grid(row=1, column=1,columnspan=2)
     master.mainloop()

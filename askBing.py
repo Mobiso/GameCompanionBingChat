dd686398002535df95de02d9bd905e7a2d9bd67d
Mobiso <95570641+Mobiso@users.noreply.github.com>
2023-06-25 16:56:54 +0200

    Ask for short answer or ask general question

--- askBing.py
+++ askBing.py
@@ -2,12 +2,17 @@ import asyncio, json
 from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
 
 
-async def askBing(activeWindow,question):
-    print("Enter question:\n")
+async def askBing(activeWindow,question,shortanswer,generalQuestion):
     activeWindow = activeWindow.replace(".exe","")
     print("Software is: " + activeWindow)
     bot = await Chatbot.create() # Passing cookies is "optional", as explained above
-    request = "Software being used: " + activeWindow + ". " + "My question is: " + question
+    if shortanswer is True:
+        question = question + ". Answer length: Short"
+    if generalQuestion is not True:
+        request = "Software being used: " + activeWindow + ". " + "My question is: " + question
+    elif generalQuestion is True:
+        request = question
+
     print(request)
     response = await bot.ask(prompt=request, conversation_style=ConversationStyle.creative, simplify_response=True)
     return response
--- gui.py
+++ gui.py
@@ -1,16 +1,15 @@
 import tkinter as tk
-
+from tkinter import *
 import activeWindow
 import askBing
 import asyncio
 import keyboard
 import json
 
-
-def ask(software, question):
+def ask(software, question,shortanswer,generalQuestion):
     print("in ask")
     if question is not None:
-        response = asyncio.run(askBing.askBing(software, question))
+        response = asyncio.run(askBing.askBing(software, question,shortanswer,generalQuestion))
         for key in response:
             if key == "text":
                 return response[key]
@@ -35,28 +34,35 @@ def answerWindow(response, question):
     answerWindow.mainloop()
 
 
-def middleHand(window, processName, question):
-    print("Entered middleHand")
+def middleHand(window, processName, question,shortanswer,generalQuestion):
     close(window)
-    answerWindow(ask(processName, question),question)
+    answerWindow(ask(processName, question,shortanswer,generalQuestion),question)
 
 
 programRunning = True
 while programRunning:
+
     print("Started")
     keyboard.wait("k")
     software = activeWindow.active_window_process_name()
-    print("K pressed")
     master = tk.Tk()
-    print("Master createdk")
     master.attributes('-topmost', True)
     master.title("Ask Bing!")
+
+    #Variables
+    shortVar = BooleanVar()
+    generalQuestion = BooleanVar()
+
     tk.Label(master, text="Question").grid(row=0)
     e1 = tk.Entry(master)
-    print("next is b1")
-    b1 = tk.Button(master, text="Ask!", command=lambda :[middleHand(master, software, e1.get())])
+    checkboxShortAnswer = tk.Checkbutton(master, text="Short answer",variable=shortVar)
+    checkboxGeneral = tk.Checkbutton(master, text="General Question", variable=generalQuestion)
+    b1 = tk.Button(master, text="Ask!", command=lambda :[middleHand(master, software, e1.get(),shortVar.get(),generalQuestion.get())])
+
     # b2 = tk.Button(master, text="Quit", command=close(master))
     e1.grid(row=0, column=1, columnspan=2)
-    b1.grid(row=1, column=0, columnspan=2)
+    b1.grid(row=1, column=0)
+    checkboxShortAnswer.grid(row=1,column=1)
+    checkboxGeneral.grid(row = 1, column=2)
     # kb2.grid(row=1, column=1,columnspan=2)
     master.mainloop()
